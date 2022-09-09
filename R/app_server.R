697c97a7f8dd37062104d31647c05b61f3d67585
Jacci <33001576+jacciz@users.noreply.github.com>
2022-09-09 17:41:25 -0500

    test

--- R/app_server.R
+++ R/app_server.R
@@ -117,71 +117,80 @@ app_server <- function( input, output, session ) {
   # Example: https://www.youtube.com/user/RodCoelho/search?query=MySQL Don't need loadDropdown as we are not deleting records
   #  -------------- Input Roast Data - Dropdowns for Form --------------------
 
-  # This checks to see if uploaded profile is valid, returns booleon
-  # valid_profile_upload <- reactive({
-  #   if (any(c(
-  #     length(input$roast_curves_upload$datapath) != 0 &
-  #     !is.null(input$roast_curves_upload) &
-  #     str_sub(input$roast_curves_upload$datapath, -5, -1) == ".alog"
-  #   ))) {
-  #     return(TRUE)
-  #   }
-  #   return(FALSE)
-  # })
 
-# Conditions we want all to be true. Returns true if all are met.
-    # is_file_valid <- any(c(length(input$roast_curves_upload$datapath) != 0 &
-    #                          !is.null(input$roast_curves_upload) &
-    #                          str_sub(input$roast_curves_upload$datapath, -5, -1) == ".alog")) # Input file must end in .alog
-    #
-    # feedbackWarning(
-    #   "roast_curves_upload", !is_file_valid,
-    #   "Input .alog"
-    # )
 
-    # df <- debounce(reactive(shared_iris$data(withSelection = TRUE)), 250)
   #  -------------- Compare multiple profiles -------------------
   many_profiles <- reactive({
-    profile <- open_profile_as_json(
-      alog_input = input$roast_curves_upload_multiple$datapath)
-    cleaned <- clean_raw_alog(profile)
-    cleaned_cross <- crosstalk::SharedData$new(cleaned)
-    cleaned_cross
-  })
 
-  # When the new button is pushed, need session object so clear out fields
-  observeEvent(input$roast_curves_upload_multiple$datapath, {
-    if (!is.null(input$roast_curves_upload_multiple)) {
+    # If false does not exist in string
+    if ("FALSE" %in% as.character(
+      lapply(input$roast_curves_upload_multiple$name,is_file_valid)) == FALSE) {
+      profiles <- lapply(input$roast_curves_upload_multiple$datapath,
+                         open_profile_as_json)
+      cleaned <-
+        do.call(dplyr::bind_rows, lapply(profiles, clean_raw_alog))
 
-      # Cross talk it!
-      one_profile <- many_profiles()$data(withSelection = TRUE)
-      display_this <- get_data_to_display_at_upload(one_profile) %>%
-        tidyr::pivot_longer(cols = 1:4, values_to = "data")
-      #
-      DT_table <- get_DT_table(one_profile)
-      mod_chart_summary_table_template_server("uploaded_data_preview", display_this)
-      mod_chart_roasting_profile_server("uploaded_profile", one_profile)
-      mod_DT_summary_table_server("summary_table", DT_table)
+      cleaned_cross <- crosstalk::SharedData$new(cleaned)
+      cleaned_cross
+
+    } else if (!is.null(input$roast_curves_upload_multiple$name) | "FALSE" %in%
+               as.character(lapply(
+                 input$roast_curves_upload_multiple$name,is_file_valid))) {
+      print("invalid")
+      showModal(modalDialog(
+        title = "Error2",
+        paste("At least one invalid .alog"),
+        easyClose = TRUE
+      ))
     }
   })
 
-  #  -------------- Examine single profiles --------------------
-  one_profile <- reactive({
-    profile <- open_profile_as_json(
-      alog_input = input$roast_curves_upload_single$datapath)
-    clean_raw_alog(profile)
+  observeEvent(input$roast_curves_upload_multiple$datapath, {
+    # print( "FALSE" %in%
+    #          as.character(lapply(
+    #            input$roast_curves_upload_multiple$name,is_file_valid)))
+    req("FALSE" %in% as.character(
+      lapply(input$roast_curves_upload_multiple$name,is_file_valid)) == FALSE)
+
+      # Cross talk it!
+      profiles <- many_profiles()$data(withSelection = TRUE)
+
+      DT_table <- get_DT_table(profiles)
+      mod_chart_roasting_profile_multiple_server("uploaded_profile_multiple", profiles)
+      mod_DT_summary_table_server("summary_table_multiple", DT_table)
+
   })
 
-  observeEvent(input$roast_curves_upload_single$datapath, {
-    if (!is.null(input$roast_curves_upload_single)) {
+  #  -------------- Examine single profile --------------------
+  one_profile <- reactive({
+    if (is_file_valid(input$roast_curves_upload_single)) {
+      profile <-
+        open_profile_as_json(alog_input = input$roast_curves_upload_single$datapath)
+      return(clean_raw_alog(profile))
+    } else if (!is.null(input$roast_curves_upload_single)) {
+      # TODO feedback doesn't work
+      shinyFeedback::feedbackWarning(
+        "roast_curves_upload",
+        !is_file_valid(input$roast_curves_upload_single),
+        text = "Input .alog"
+      )
+      showModal(modalDialog(
+        title = "Error",
+        paste("Upload valid .alog"),
+        easyClose = TRUE
+      ))
+    }
+  })
 
+  observeEvent(one_profile(), {
+    # Require these to be valid. If they are all valid, code will finally proceed.
+    req(is_file_valid(input$roast_curves_upload_single))
       display_this <- get_data_to_display_at_upload(one_profile()) %>%
         tidyr::pivot_longer(cols = 1:4, values_to = "data")
       DT_table <- get_DT_table(one_profile())
       mod_chart_summary_table_template_server("uploaded_data_preview_single", display_this)
       mod_chart_roasting_profile_server("uploaded_profile_single", one_profile())
       mod_DT_summary_table_server("summary_table_single", DT_table)
-    }
   })
 
 }
